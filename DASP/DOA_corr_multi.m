8ee067d7e62b94a7cfa434687d68c297e8c7758d
TristanMarchant <32941408+TristanMarchant@users.noreply.github.com>
2018-10-26 12:34:44 +0100

    Session1_Finished

--- DASP/DOA_corr_multi.m
+++ DASP/DOA_corr_multi.m
@@ -21,7 +21,8 @@ delay_groundtruth = m1_s_arrival - m2_s_arrival; %or abs value?
 
 %---- COPIED FROM CREATE_MICSIGS.M ----------------%
 speechfiles{1} = 'speech1.wav';
-%speechfiles{2} = 'speech2.wav';
+speechfiles{2} = 'speech2.wav';
+speechfiles{3} = 'speech3.wav';
 noisefiles{1} = 'Babble_noise1.wav';
 %noisefiles{2} = 'Babble_noise1.wav';
 
@@ -58,27 +59,49 @@ for i=1:nb_noisefiles
 end
 
 mic = zeros(nb_min, nb_mics);
+DOA_est = zeros(1,nb_audiosrc);
 
-for i=1:nb_mics
+for j=1:nb_audiosrc
     
-    for j=1:nb_audiosrc
+    for i=1:nb_mics
         mic(:,i) = mic(:,i) + fftfilt(RIR_sources(:, i, j), speech_resampled{j});
     end
     
+    [r, lags] = xcorr(mic(:,1), mic(:,2)); % adjusted for accuracte autocorr, longer computing though
+    [val, idx] = max(r); %value and index at lags of highest value
+    est_delay = lags(idx)/fs_RIR;
+    if (est_delay*c/d) > 1
+        DOA_est(j) = acos(1)*180/pi;
+    else
+        DOA_est(j) = acos(est_delay*c/d)*180/pi; %Tested with ground truth should be done with estimate
+    end
     
-    for j=1:nb_noisesrc
-        mic(:,i) = mic(:,i) + fftfilt( RIR_noise(:, i, j), noise_resampled{j});
+    for i=1:nb_mics
+        mic(:,i) = 0;
     end
 end
 
-% --------- END OF COPY ---------------%
-% --------- CROSS CORRELATION ---------%
-
-[r, lags] = xcorr(mic(:,1), mic(:,2)); % adjusted for accuracte autocorr, longer computing though
-[val, idx] = max(r); %value and index at lags of highest value
-est_delay = lags(idx)/fs_RIR; 
-
-% --------- COPY OF TDOA TILL THIS POINT ------
-
-DOA_est = acos(est_delay*c/d)*180/pi; %Tested with ground truth should be done with estimate
 save DOA_est
+% for i=1:nb_mics
+%    
+%     for j=1:nb_audiosrc
+%         mic(:,i) = mic(:,i) + fftfilt(RIR_sources(:, i, j), speech_resampled{j});
+%     end
+%     
+%     
+%     for j=1:nb_noisesrc
+%         mic(:,i) = mic(:,i) + fftfilt( RIR_noise(:, i, j), noise_resampled{j});
+%     end
+% end
+% 
+% % --------- END OF COPY ---------------% --------- CROSS CORRELATION
+% % ---------%
+% 
+% [r, lags] = xcorr(mic(:,1), mic(:,2)); % adjusted for accuracte autocorr, longer computing though
+% [val, idx] = max(r); %value and index at lags of highest value
+% est_delay = lags(idx)/fs_RIR; 
+% 
+% % --------- COPY OF TDOA TILL THIS POINT ------
+% 
+% DOA_est = acos(est_delay*c/d)*180/pi; %Tested with ground truth should be done with estimate
+% save DOA_est
